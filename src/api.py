667575a9e9ea8edbc8fbad161bb1efc31c814625
shengmin.leong <leongshengmin@gmail.com>
2020-09-18 23:39:05 +0800

    [New] Initial checkin.

---
+++ src/api.py
@@ -0,0 +1,50 @@
+import logging
+import traceback
+
+from flask import Flask
+
+from accessors.urls_tab import MysqlUrlsTabAccessor
+from common.exceptions import ShortUrlException
+from conf import Conf
+from managers.default import DefaultShortUrlManager
+from managers.generators.auto_inc import AutoIncShortKeyGenerator
+from routes import add_short_url_routes
+from views.shorten import ShortUrlsView
+
+
+def init_configs(_app):
+    _app.config.update(Conf['flask'])
+
+
+def create_urls_view():
+    urls_tab_accessor = MysqlUrlsTabAccessor()
+    generator = AutoIncShortKeyGenerator(random_step=100)
+    short_url_manager = DefaultShortUrlManager(urls_tab_accessor, generator)
+    short_urls_view = ShortUrlsView(short_url_manager)
+    return short_urls_view
+
+
+def add_routes(_app):
+    add_short_url_routes(_app, view=create_urls_view())
+
+
+def create_app():
+    _app = Flask(__name__)
+    init_configs(_app)
+    add_routes(_app)
+    return _app
+
+
+app = create_app()
+
+
+@app.errorhandler(Exception)
+def exception_handler(error):
+    """
+    Handle all uncaught exceptions.
+    """
+    if isinstance(error, (ShortUrlException,)):
+        return {'error': error.message}, 500
+
+    logging.error(''.join(traceback.format_exception(type(error), error, error.__traceback__)))
+    return {'error': 'system busy'}, 500
---
+++ src/managers/default.py
@@ -0,0 +1,54 @@
+from accessors import UrlsTabAccessor
+from managers import ShortUrlManager, NotFoundException
+from managers.generators import UniqueShortKeyGenerator
+from utils.redis import redis_client
+
+class DefaultShortUrlManager(ShortUrlManager):
+    def __init__(self, urls_tab_accessor: UrlsTabAccessor, short_key_generator: UniqueShortKeyGenerator):
+        super().__init__(urls_tab_accessor, short_key_generator)
+
+    def resolve(self, short_key: str) -> str:
+        """
+        Return the latest source url for the short key.
+        """
+
+        redis = redis_client()
+        cached_url = redis.get(short_key)
+
+        # cache hit
+        if cached_url:
+            return cached_url.decode("utf-8")
+
+        # cache miss, query DB
+        db_url = self.urls_tab_accessor.find_last_by_short_key(short_key)
+        if db_url is None:
+            raise NotFoundException(f'url not found for short_key: {short_key}')
+        return db_url
+
+    def create(self, url: str) -> str:
+        """
+        Return a short key for the url.
+        """
+        redis = redis_client()
+        cached_short_key = redis.get(url)
+
+        # cache hit
+        if cached_short_key:
+            return cached_short_key
+
+        short_key, matched = self.urls_tab_accessor.find_match_by_url(url)
+        if matched == url:
+            return short_key
+
+        # just create a new short_key
+        generated_short_key = self._generate_new_short_key(url)
+
+        # add generated key to cache
+        redis.setnx(url, generated_short_key)
+
+        return generated_short_key
+
+    def _generate_new_short_key(self, url):
+        short_key = self.short_key_generator.generate(url)
+        self.urls_tab_accessor.create(short_key, url)
+        return short_key
